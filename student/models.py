14ce406c4d9841e0f4a60fa8fe121e376c91502b
Jawad Hussain <jawadduet@gmail.com>
2024-07-25 12:34:40 +0500

    Fix enrollment date

--- student/models.py
+++ student/models.py
@@ -12,7 +12,7 @@ class Student(models.Model):
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     address = models.TextField()
-    enrollment_date = models.DateField(validators=[validate_year])
+    enrollment_date = models.DateField(validators=[validate_enrollment_date])
     
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
