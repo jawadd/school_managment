78582b486df3bb14edb7ed48f0c900fb4d9c960a
Jawad Hussain <jawadduet@gmail.com>
2024-07-25 13:00:55 +0500

    fix enrollment date on admin

--- student/models.py
+++ student/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from uuid import uuid4
 from .validators import validate_enrollment_date
+from django.core.exceptions import ValidationError
+from datetime import date
 from school.models import School
 
 class Student(models.Model):
@@ -12,13 +14,15 @@ class Student(models.Model):
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     address = models.TextField()
-    enrollment_date = models.DateField(validators=[validate_enrollment_date])
+    enrollment_date = models.DateField()
     
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
     def clean(self):
         super().clean()
         # Validate enrollment_date against date_of_birth
-        if self.enrollment_date and self.date_of_birth:
-            validate_enrollment_date(self.enrollment_date, self.date_of_birth)
+        if self.enrollment_date < self.date_of_birth:
+            raise ValidationError("Enrollment date cannot be before the date of birth.")
+        if self.enrollment_date > date.today():
+            raise ValidationError("Enrollment date cannot be in the future.")
 
--- student/serializers.py
+++ student/serializers.py
@@ -1,7 +1,21 @@
 from rest_framework import serializers
 from .models import Student
+from datetime import date
 
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
+
         fields = ['id', 'school', 'first_name', 'last_name', 'date_of_birth', 'email', 'phone_number', 'address', 'enrollment_date']
+
+        def validate(self, data):
+            date_of_birth = data.get('date_of_birth')
+            enrollment_date = data.get('enrollment_date')
+
+            
+            if enrollment_date < date_of_birth:
+                raise serializers.ValidationError("Enrollment date cannot be before the date of birth.")
+            if enrollment_date > date.today():
+                raise serializers.ValidationError("Enrollment date cannot be in the future.")
+        
+            return data
