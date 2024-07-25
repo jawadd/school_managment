b52074bfcced6548f2a5d8f77a2fe007d4bbe8e5
Jawad Hussain <jawadduet@gmail.com>
2024-07-25 13:04:02 +0500

    Fix validation error with serializer

--- student/serializers.py
+++ student/serializers.py
@@ -8,14 +8,14 @@ class StudentSerializer(serializers.ModelSerializer):
 
         fields = ['id', 'school', 'first_name', 'last_name', 'date_of_birth', 'email', 'phone_number', 'address', 'enrollment_date']
 
-        def validate(self, data):
-            date_of_birth = data.get('date_of_birth')
-            enrollment_date = data.get('enrollment_date')
+    def validate(self, data):
+        date_of_birth = data.get('date_of_birth')
+        enrollment_date = data.get('enrollment_date')
 
             
-            if enrollment_date < date_of_birth:
-                raise serializers.ValidationError("Enrollment date cannot be before the date of birth.")
-            if enrollment_date > date.today():
-                raise serializers.ValidationError("Enrollment date cannot be in the future.")
+        if enrollment_date < date_of_birth:
+            raise serializers.ValidationError("Enrollment date cannot be before the date of birth.")
+        if enrollment_date > date.today():
+            raise serializers.ValidationError("Enrollment date cannot be in the future.")
         
-            return data
+        return data
