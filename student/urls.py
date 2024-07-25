f24cc4772dcffb16cfc8cceab2ac886a4bac39c5
Jawad Hussain <jawadduet@gmail.com>
2024-07-25 17:06:53 +0500

    Add student CRUD

--- student/urls.py
+++ student/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import StudentViewSet
-from .views import StudentList
+from .views import StudentList, StudentCreate, StudentUpdate, StudentDelete
+from .views import StudentList,HomeView
 from . import views
 
 router = DefaultRouter()
 router.register(r'students', views.StudentViewSet, basename='student')
 
 urlpatterns = [
-    path('', StudentList.as_view(), name='student_list'),  # For rendering the student list
+    path('', HomeView.as_view(), name='home'), 
+    path('students/', StudentList.as_view(), name='student_list'),
+    path('create/', StudentCreate.as_view(), name='student_create'),
+    path('update/<uuid:pk>/', StudentUpdate.as_view(), name='student_update'),
+    path('delete/<uuid:pk>/', StudentDelete.as_view(), name='student_delete'),
     
 ]
--- student/views.py
+++ student/views.py
@@ -3,11 +3,49 @@ from rest_framework import viewsets
 from django.views.generic import ListView
 from .models import  Student
 from .serializers import StudentSerializer
+from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from .models import Student
+from django import forms
 
+
+class HomeView(TemplateView):
+    template_name = 'student/index.html'
+
+class StudentForm(forms.ModelForm):
+    date_of_birth = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+    enrollment_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+    class Meta:
+        model = Student
+        fields = ['school', 'first_name', 'last_name', 'date_of_birth', 'email', 'phone_number', 'address', 'enrollment_date']
 class StudentList(ListView):
     model = Student
-    template_name = 'student/index.html'  # Ensure this path is correct
+    template_name = 'student/students_list.html'
     context_object_name = 'students'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.get_queryset().exists():
+            context['school'] = self.get_queryset().first().school
+        return context
+
+class StudentCreate(CreateView):
+    model = Student
+    form_class = StudentForm
+    template_name = 'student/student_form.html'
+    success_url = reverse_lazy('student_list')
+
+class StudentUpdate(UpdateView):
+    model = Student
+    form_class = StudentForm
+    template_name = 'student/student_form.html'
+    success_url = reverse_lazy('student_list')
+
+class StudentDelete(DeleteView):
+    model = Student
+    template_name = 'student/student_confirm_delete.html'
+    success_url = reverse_lazy('student_list')
 
 
 
